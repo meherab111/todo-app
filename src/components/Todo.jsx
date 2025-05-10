7d4dc98b2c2c305ea8b9b6f3097c6dcb330fa8e9
meherab111 <mehrabborno@gmail.com>
2025-05-11 01:08:19 +0600

    updated

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,58 +7,65 @@ export const Todo = () => {
 
   const [formData, setFormData] = useState([]);
 
+  const handleInputValue = (value) => {
+    setUserInput(value);
+  };
 
-    const handleInputValue = (value) => {
-        setUserInput(value)
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (!userInput) return;
+
+    if (formData.includes(userInput)) {
+      setUserInput("");
+      return;
     }
 
-    const handleFormSubmit = (event) => {
-        event.preventDefault()
+    setFormData((prev) => [...prev, userInput]);
 
-        if(!userInput) return;
-
-        if(formData.includes(userInput)){
-            setUserInput("")
-            return;
-        } 
-
-        setFormData((prev) => [...prev, userInput])
-        
-        setUserInput("")
-        
-    }
+    setUserInput("");
+  };
 
   return (
     <section className="todo-container">
       <header>
         <h1>Todo List</h1>
-        <section className="form">
-          <form onSubmit={handleFormSubmit}>
-            <div>
-              <input type="text" className="todo-input" autoComplete="off" value={userInput} onChange={(event) => handleInputValue(event.target.value)}/>
-            </div>
-            <div>
-              <button type="submit" className="todo-button">
-                Add Task
-              </button>
-            </div>
-          </form>
-        </section>
-        <section className="myUnOrdList">
-        <ul>
-            {
-                formData.map((currElem, index) => {
-                    return(<li key={index}>
-
-                        <span>{currElem}</span>
-                        <button className="check-btn"><FaCheckCircle /></button>
-                        <button className="delete-btn"><MdDeleteForever /></button>
-                    </li>)
-                })
-            }
-        </ul>
-        </section>
       </header>
+      <section className="form">
+        <form onSubmit={handleFormSubmit}>
+          <div>
+            <input
+              type="text"
+              className="todo-input"
+              autoComplete="off"
+              value={userInput}
+              onChange={(event) => handleInputValue(event.target.value)}
+            />
+          </div>
+          <div>
+            <button type="submit" className="todo-button">
+              Add Task
+            </button>
+          </div>
+        </form>
+      </section>
+      <section className="myUnOrdList">
+        <ul>
+          {formData.map((currElem, index) => {
+            return (
+              <li className="todo-item" key={index}>
+                <span>{currElem}</span>
+                <button className="check-btn">
+                  <FaCheckCircle />
+                </button>
+                <button className="delete-btn">
+                  <MdDeleteForever />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </section>
   );
 };
